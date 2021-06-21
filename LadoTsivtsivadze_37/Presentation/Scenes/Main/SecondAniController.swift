597770716a6f4bed3b069893ef223f5d119552e6
Lado <Tsivtsivadzelado@yahoo.com>
2021-06-21 11:27:35 -0700

    progressing in second controller

--- LadoTsivtsivadze_37/Presentation/Scenes/Main/SecondAniController.swift
+++ LadoTsivtsivadze_37/Presentation/Scenes/Main/SecondAniController.swift
@@ -8,10 +8,55 @@
 import UIKit
 
 class SecondAniController: UIViewController {
-
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var portraitBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if UIDevice.current.orientation.isPortrait {
+            //testLabel.isHidden = true
+        }
+        else {
+            //testLabel.isHidden = false
+        }
 
 
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        
+        if UIDevice.current.orientation.isLandscape {
+            print("Landscape")
+
+            UIView.animateKeyframes(withDuration: 1, delay: 0, options: .allowUserInteraction) {
+                UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.5) {
+                    self.headlineLabel.layer.opacity = 0.0
+                    self.portraitBtn.layer.opacity = 0.0
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
+                    <#code#>
+                }
+        
+            } completion: { (success) in
+                
+            }
+
+            
+            
+        }
+        else {
+            print("Portrait")
+            UIView.animate(withDuration: 1) { [weak self] in
+                guard let self = self else { return }
+                self.headlineLabel.layer.opacity = 1.0
+                self.portraitBtn.layer.opacity = 1.0
+                //self.testLabel.layer.opacity = 0.0
+            }
+        }
     }
 }
