792df1aeffa2add90c623b49f912846983d421c2
Lado <Tsivtsivadzelado@yahoo.com>
2021-06-21 04:30:21 -0700

    started fourth controller

--- LadoTsivtsivadze_37/Presentation/Scenes/Main/FourthAniController.swift
+++ LadoTsivtsivadze_37/Presentation/Scenes/Main/FourthAniController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class FourthAniController: UIViewController {
+    
+    private var greenBox: UIView = {
+        let frame = CGRect(x: CenterPoints.x,
+                           y: CenterPoints.y,
+                           width: 70,
+                           height: 70)
+        
+        let view = UIView(frame: frame)
+        view.backgroundColor = .green
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
